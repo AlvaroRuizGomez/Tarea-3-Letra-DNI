0a97cc89df0567932ca922da5ac4bb23b5314d1d
@lvaroRuiz <alvarorrgomez@gmail.com>
2021-01-30 20:57:44 +0100

    Se crean funciones

--- DigitVerifyFunction.js
+++ DigitVerifyFunction.js
@@ -2,27 +2,42 @@
  * POLICIA saber la letra de tu dni
  */
 
-//  Base de datos
+// --------   Scope Global  -----------
 // Array
 const LETRAS_DNI = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E']
-// preguntar al usuario, el numero de su dni
-let numeroDNI = prompt("No sabes tu letra de DNI? Escribe quí tus números")
 
-// bucle
-while(numeroDNI != 'salir') {
-    // resultado de la letra 
-    const resto = numeroDNI % 23
-    // que tipo de datos permitimos o no
+var validarOk = true;
+var resto = 0
+var numeroDNI = 0
+
+// --------   Scope Function  ---------
+function validarEntrada(){
     switch(true) {
+        case isNaN(numeroDNI):
         case numeroDNI < 0:
         case numeroDNI.length != 8:
         case numeroDNI.length == '':
             alert('ERROR!!!')
-            numeroDNI = 'salir'
+            validarOk = false
             break
         default:
-            alert(`DNI: ${numeroDNI}-${LETRAS_DNI[resto]}`)
-            numeroDNI = 'salir'
+            resto = numeroDNI % 23
+            mostrarDNI()
             break
     }
+}
+
+function mostrarDNI() {
+    alert(`DNI: ${numeroDNI}-${LETRAS_DNI[resto]}`)
+    validarOk = true
+}
+
+// --------   Scope Code  ---------
+// preguntar al usuario, el numero de su dni la primera vez antes de entrar al bucle.
+numeroDNI = prompt("No sabes tu letra de DNI? Escribe aquí tus números. Para terminar introduzca : salir")
+
+// bucle mientras se quiere introducir datos
+while(validarOk = true && numeroDNI != 'salir') {
+    validarEntrada() 
+    numeroDNI = prompt("No sabes tu letra de DNI? Escribe aquí tus números. Para terminar introduzca : salir")
 }
